a90363c07ab767a6710a92d2373fd48e60bd751d
vincentkovalski <kovall89@gmail.com>
2022-07-16 12:00:37 +0300

    Initial Commit

---
+++ Collection-homework-14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection-homework-14
+//
+//  Created by Виктор Ковалевский on 16.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
